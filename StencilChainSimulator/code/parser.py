88331a3b9077c0be6ce2db01953ee179515fcb0c
andreas <mail@andreaskuster.ch>
2018-10-01 16:24:52 +0200

    Structure for later implementation of the parse. (First we do it using a dummy input instead of the fastwaves example.

---
+++ StencilChainSimulator/code/parser.py
@@ -0,0 +1,10 @@
+class Parser:
+
+    def __init__(self, sourcepath):
+        self.sourcepath = sourcepath
+
+
+'''
+    Basic version:  parse same input as we parsed in StencilFPGA
+    Extension:      parse actual input from the cosmo model
+'''
