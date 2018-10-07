bd45c5f47257fa0f36a6f8c319ba436964cf9482
andreas <mail@andreaskuster.ch>
2018-10-07 22:12:45 +0200

    Config implementation for kernel class. Manually added simple test kernel for debugging.

--- StencilChainSimulator/code/kernel.py
+++ StencilChainSimulator/code/kernel.py
@@ -1,11 +1,51 @@
+import os
+import ast
+import json
 
 class Kernel:
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, name, kernel_string):
 
+        # store arguments
+        self.name = name  # kernel name
+        self.kernel_string = kernel_string  # raw kernel string input
+
+        # read static parameters from config
+        self.config_path = "kernel.config"
+        self.op_latency = None
+        self.parse_config()
+
+    ''' 
+        import config 
     '''
+    def parse_config(self):
+
+        # check file exist and readable
+        if not os.access(self.config_path, os.R_OK):
+            raise Exception("Config does not exist or is not readable.")
+
+        # open the file read-only
+        file_handle = open(self.config_path, "r")
+
+        # try to parse it
+        config = json.loads(file_handle.read())  # type: dict
+
+        # close the file handle
+        file_handle.close()
+
+        # fill in information
+        self.op_latency = config["op_latency"]
+
+    ''' 
+        analyse input kernel string 
+    '''
+
+    def parse_inputs(self):
+        return
+
+    '''  
         interface for FPGA-like execution (get called from the scheduler)
+    
             - read: 
                     - saturation phase: read unconditionally
                     - execution phase: read all inputs iff they are available
@@ -21,6 +61,7 @@ class Kernel:
                     - True  iff successful
                     - False otherwise 
     '''
+
     def try_read(self):
         raise NotImplementedError()
 
@@ -41,3 +82,11 @@ class Kernel:
     '''
     def diagnostics(self):
         raise NotImplementedError()
+
+    '''
+        simple test kernel for debugging
+    '''
+
+
+if __name__ == "__main__":
+    kernel = Kernel("res1", "auto res = res1(i,j,k) + inC(i,j,k);")
