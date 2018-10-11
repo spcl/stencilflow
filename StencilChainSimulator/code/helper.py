12c4d03517686babcd4c89a3209e1424849c2402
andreas <mail@andreaskuster.ch>
2018-10-11 11:20:49 +0200

    Outsource methods that are used in many classes, but are not actually a core part of them.

---
+++ StencilChainSimulator/code/helper.py
@@ -0,0 +1,27 @@
+import os
+import json
+
+
+class Helper:
+
+    def __init__(self):
+        return
+
+    @staticmethod
+    def parse_config(config_path):
+
+        # check file exist and readable
+        if not os.access(config_path, os.R_OK):
+            raise Exception("Config does not exist or is not readable.")
+
+        # open the file read-only
+        file_handle = open(config_path, "r")
+
+        # try to parse it
+        config = json.loads(file_handle.read())  # type: dict
+
+        # close the file handle
+        file_handle.close()
+
+        # return dict
+        return config
