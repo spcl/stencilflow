85b1341c331ac8c03f8ffea21459a8a484678a21
andreas <mail@andreaskuster.ch>
2018-11-18 19:21:10 +0100

    Bug fix.

--- StencilChainSimulator/code/helper.py
+++ StencilChainSimulator/code/helper.py
@@ -10,7 +10,7 @@ class Helper:
     def parse_json(config_path):
 
         # open file in with-clause, to ensure proper file closing even in the event of an exception
-        with open("config_path", "r") as file_handle:
+        with open(config_path, "r") as file_handle:
             # try to parse it
             config = json.loads(file_handle.read())  # type: dict
 
