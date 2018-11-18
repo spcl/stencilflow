921ce26203c137bb10629ac6abfa43c660db089e
andreas <mail@andreaskuster.ch>
2018-11-18 19:20:49 +0100

    Adapt to new standard.

--- StencilChainSimulator/code/helper.py
+++ StencilChainSimulator/code/helper.py
@@ -1,27 +1,18 @@
-import os
 import json
 
 
 class Helper:
 
     def __init__(self):
-        return
+        pass
 
     @staticmethod
     def parse_json(config_path):
 
-        # check file exist and readable
-        if not os.access(config_path, os.R_OK):
-            raise Exception("Config does not exist or is not readable.")
-
-        # open the file read-only
-        file_handle = open(config_path, "r")
-
-        # try to parse it
-        config = json.loads(file_handle.read())  # type: dict
-
-        # close the file handle
-        file_handle.close()
+        # open file in with-clause, to ensure proper file closing even in the event of an exception
+        with open("config_path", "r") as file_handle:
+            # try to parse it
+            config = json.loads(file_handle.read())  # type: dict
 
         # return dict
         return config
