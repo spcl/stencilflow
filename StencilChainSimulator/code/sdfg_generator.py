d56cf404cf71da7a52af4864bcb2df859a006c73
Johannes de Fine Licht <johannes@musicmedia.dk>
2019-02-18 17:21:22 +0100

    Generate reader modules

--- StencilChainSimulator/code/sdfg_generator.py
+++ StencilChainSimulator/code/sdfg_generator.py
@@ -1,16 +1,95 @@
 import argparse
+import collections
 import os
 import re
+from dapp.memlet import Memlet
 from dapp.sdfg import SDFG
-from kernel_chain_graph import KernelChainGraph
+from dapp.types import ScheduleType, StorageType
+from kernel_chain_graph import KernelChainGraph, NodeType
+
+DATA_TYPE = float
+ITERATORS = ["i", "j", "k"]
+
+
+def iteration_space(chain):
+    if len(chain.dimensions) > 3:
+        return collections.OrderedDict([
+            ("i" + str(i), "0:" + str(d))
+            for i, d in enumerate(chain.dimensions)
+        ])
+    else:
+        return collections.OrderedDict([
+            (ITERATORS[i], "0:" + str(d))
+            for i, d in enumerate(chain.dimensions)
+        ])
+
+def stream_name(src_name, dst_name):
+    return src_name + "_to_" + dst_name
+
+
+def add_array(sdfg, state, node, chain):
+
+    sdfg.add_array(
+        node.name,
+        chain.dimensions,
+        DATA_TYPE,
+        storage=StorageType.FPGA_Global,
+        transient=True)
+
+    access_node = state.add_read(node.name)
+
+    iterators = iteration_space(chain)
+
+    entry, exit = state.add_map(
+        "read_" + node.name, iterators, schedule=ScheduleType.FPGA_Device)
+
+    # Sort to get deterministic output
+    outputs = sorted(node.outputs.keys())
+
+    tasklet_code = "\n".join(["{} = memory".format(n) for n in outputs])
+
+    tasklet = state.add_tasklet("read_" + node.name, {"memory"}, outputs,
+                                tasklet_code)
+
+    state.add_memlet_path(
+        access_node,
+        entry,
+        tasklet,
+        dst_conn="memory",
+        memlet=Memlet.simple(node.name, ", ".join(iterators.keys())))
+
+    for dst_name in outputs:
+        data_name = stream_name(node.name, dst_name)
+        sdfg.add_stream(
+            data_name,
+            DATA_TYPE,
+            buffer_size=node.outputs[dst_name].maxsize,
+            storage=StorageType.FPGA_Local,
+            transient=True)
+        write_node = state.add_write(data_name)
+        state.add_memlet_path(
+            tasklet,
+            exit,
+            write_node,
+            src_conn=dst_name,
+            memlet=Memlet.simple(data_name, "0"))
+
 
 
 def generate_sdfg(name, chain):
 
     sdfg = SDFG(name)
 
+    state = sdfg.add_state("compute")
+
+    memories = {}
+
+    # Proce
     for node in chain.graph.nodes():
-        import pdb; pdb.set_trace()
+        if node.node_type == NodeType.INPUT:
+            add_array(sdfg, state, node, chain)
+
+    sdfg.draw_to_file()
 
 
 if __name__ == "__main__":
@@ -25,6 +104,6 @@ if __name__ == "__main__":
 
     chain = KernelChainGraph(args.stencil_file)
 
-    chain.plot_graph(name + ".pdf")
+    # chain.plot_graph(name + ".pdf")
 
-    # generate_sdfg(name, chain)
+    generate_sdfg(name, chain)
