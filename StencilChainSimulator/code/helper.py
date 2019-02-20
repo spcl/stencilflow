9c4ec59e4323a848419dad49b18e3204e496ce1a
andreas <mail@andreaskuster.ch>
2019-02-20 12:04:17 +0100

    Move static functions to Helper class for proper reuse.

--- StencilChainSimulator/code/helper.py
+++ StencilChainSimulator/code/helper.py
@@ -16,3 +16,48 @@ class Helper:
 
         # return dict
         return config
+
+    @staticmethod
+    def max_buffer(buffer):
+        max_buf = [0, 0, 0]
+        for buf in buffer:
+            entry = buffer[buf]
+            if entry[0] > max_buf[0]:
+                max_buf = entry
+            elif entry[0] == max_buf[0] and entry[1] > max_buf[1]:
+                max_buf = entry
+            elif entry[0] == max_buf[0] and entry[1] == max_buf[1] and entry[2] > max_buf[2]:
+                max_buf = entry
+        return max_buf
+
+    @staticmethod
+    def max_list_entry(buf):
+        max_buf = [0, 0, 0]
+        for entry in buf:
+            if entry[0] > max_buf[0]:
+                max_buf = entry
+            elif entry[0] == max_buf[0] and entry[1] > max_buf[1]:
+                max_buf = entry
+            elif entry[0] == max_buf[0] and entry[1] == max_buf[1] and entry[2] > max_buf[2]:
+                max_buf = entry
+        return max_buf
+
+    @staticmethod
+    def min_list_entry(buf):
+        min_buf = buf[0]
+        for entry in buf:
+            if entry[0] < min_buf[0]:
+                min_buf = entry
+            elif entry[0] == min_buf[0] and entry[1] < min_buf[1]:
+                min_buf = entry
+            elif entry[0] == min_buf[0] and entry[1] == min_buf[1] and entry[2] < min_buf[2]:
+                min_buf = entry
+        return min_buf
+
+    @staticmethod
+    def list_add_cwise(list1, list2):
+        return list(map(lambda x, y: x + y, list1, list2))
+
+    @staticmethod
+    def list_subtract_cwise(list1, list2):
+        return list(map(lambda x, y: x - y, list1, list2))
