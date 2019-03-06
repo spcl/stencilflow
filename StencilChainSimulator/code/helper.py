b07ba0065871a89542f6fa7e605629446ed7b537
andreas <mail@andreaskuster.ch>
2019-03-06 09:18:34 +0100

    Merge helper methods into single class.

--- StencilChainSimulator/code/helper.py
+++ StencilChainSimulator/code/helper.py
@@ -80,3 +80,19 @@ class Helper:
                 return False
         else:
             return False
+
+    @staticmethod
+    def stencil_memory_index(indices, dimensions):
+        if len(indices) != len(dimensions):
+            raise ValueError("Dimension mismatch")
+        factor = 1
+        res = 0
+        for i, d in zip(reversed(indices), reversed(dimensions)):
+            res += i * factor
+            factor *= d
+        return res
+
+    @staticmethod
+    def stencil_distance(a, b, dimensions):
+        return abs(Helper.stencil_memory_index(a) - Helper.stencil_memory_index(b))
+
