92bde1d1060ecc0a4dec2b2742974f7dea2bc776
andreas <mail@andreaskuster.ch>
2019-04-30 12:33:11 +0200

    Add Intel OpenCL FPGA test designs (e.g. 2D filter inc poundary condition in a fully pipelined stream)

---
+++ IntelOpenCLFPGATesting/lab9_latency_sub/synth_emu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#SBATCH -N 1      # nodes requested
+#SBATCH -o synth_emu_outfile  # send stdout to outfile
+#SBATCH -e synth_emu_errfile  # send stderr to errfile
+#SBATCH -t 00:30:00  # time requested in hour:minute:second
+#SBATCH -p long
+#SBATCH -A hpc-prf-cosmo
+#SBATCH --mail-type all
+#SBATCH --mail-user kustera@ethz.ch
+
+module load intelFPGA_pro/18.1.1_max nalla_pcie/18.1.1_max
+aoc -v -march=emulator -board=p520_max_sg280l channels.cl
