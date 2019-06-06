b9cdf5159e28ec21b03ceadbd605428afff29e22
andreas <mail@andreaskuster.ch>
2019-06-06 09:57:08 +0200

    Add execution cycle counter.

--- StencilChainSimulator/code/simulator.py
+++ StencilChainSimulator/code/simulator.py
@@ -80,8 +80,18 @@ class Simulator:
         self.initialize()
 
         # run simulation
+        PC = 0
         while not self.all_done():
             self.step_execution()
+            PC += 1
+            for input in self.input_nodes:
+                print("input:{}, PC: {}".format(input, self.input_nodes[input].program_counter))
+            for kernel in self.kernel_nodes:
+                print("kernel:{}, PC: {}".format(kernel, self.kernel_nodes[kernel].program_counter))
+            for output in self.output_nodes:
+                print("output:{}, PC: {}".format(output, self.output_nodes[output].program_counter))
+
+        print("Simulation completed after {} cycles.".format(PC))
 
         self.finalize()
 
