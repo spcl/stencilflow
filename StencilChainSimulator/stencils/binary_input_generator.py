7f611f70ea81c14bc2754d695b5c2a24c534bf7c
andreas <mail@andreaskuster.ch>
2019-07-21 16:55:18 +0200

    Replace hardcoded values by command line arguments.

--- StencilChainSimulator/stencils/binary_input_generator.py
+++ StencilChainSimulator/stencils/binary_input_generator.py
@@ -1,34 +1,48 @@
+#!/usr/bin/env python3
+import argparse
 import numpy as np
 
+parser = argparse.ArgumentParser()
+parser.add_argument("filename")
+parser.add_argument("N")
+parser.add_argument("generator", choices=["const", "random", "incr"], default="random")
+parser.add_argument("fileextension", choices=["csv", "bin", "dat"], default="dat")
+parser.add_argument("datatype", choices=["float32", "float64"], default="float64")
+parser.add_argument("-rand_lo")
+parser.add_argument("-rand_hi")
+parser.add_argument("-const_val")
+args = parser.parse_args()
+
 # general parameters
-_N = 4*4*4
-_GENERATOR = "const"  # {const, random, incr}
-_FILENAME = "inC"
-_DATA_TYPE = "dat"  # {csv, bin, dat}
+_N = int(eval(args.N))
+_GENERATOR = args.generator  # {const, random, incr}
+_FILENAME = args.filename
+_FILE_EXTENSION = args.fileextension  # {csv, bin, dat}
+_DATA_TYPE = np.dtype(args.datatype)
 # random
-_LOWER_BOUND = 0.0
-_UPPER_BOUND = 10.0
+_LOWER_BOUND = float(args.rand_lo) if args.rand_lo is not None else 0.0
+_UPPER_BOUND = float(args.rand_hi) if args.rand_hi is not None else 0.0
 # constant
-_CONSTANT = 3.0
+_CONSTANT = float(args.const_val) if args.const_val is not None else 0.0
 
 # generate data
 if _GENERATOR == "random":
     # random data
-    data = np.random.uniform(low=_LOWER_BOUND, high=_UPPER_BOUND, size=_N)
+    data = np.array(np.random.uniform(low=_LOWER_BOUND, high=_UPPER_BOUND, size=_N), dtype=_DATA_TYPE)
 elif _GENERATOR == "const":
     # constant value
-    data = np.array([_CONSTANT]*_N)
+    data = np.array([_CONSTANT]*_N, dtype=_DATA_TYPE)
 elif _GENERATOR == "incr":
     # increasing value
-    data = np.arange(0.0, _N, dtype="float64")
+    data = np.arange(0.0, _N, dtype=_DATA_TYPE)
 else:
     print("Generator {} not supported. Exit.".format(_GENERATOR))
     exit()
 
 # write data to file
-filename = _FILENAME + "." + _DATA_TYPE
+filename = _FILENAME + "." + _FILE_EXTENSION
 
-if _DATA_TYPE == "csv":
+if _FILE_EXTENSION == "csv":
     np.savetxt(filename, [data], delimiter=",")
-elif _DATA_TYPE == "bin" or _DATA_TYPE == "dat":
-    data.astype('float64').tofile(filename)
+elif _FILE_EXTENSION == "bin" or _FILE_EXTENSION == "dat":
+    data.astype(_DATA_TYPE).tofile(filename)
