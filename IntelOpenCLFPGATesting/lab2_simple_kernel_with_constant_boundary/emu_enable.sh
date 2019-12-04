12729dd1be74d0b227fb13e1d2e46ef6a396d99c
kustera <kustera@student.ethz.ch>
2019-12-04 07:15:25 -0500

    Move manual Intel FPGA OpenCL examples into the new project structure.

--- IntelOpenCLFPGATesting/lab2_simple_kernel_with_constant_boundary/emu_enable.sh
+++
@@ -1,2 +0,0 @@
-module load intelFPGA_pro/18.1.1_max nalla_pcie/18.1.1_max
-export CL_CONTEXT_EMULATOR_DEVICE_INTELFPGA=p520_max_sg280l
--- IntelOpenCLFPGATesting/lab6_latency_add/simple_compile.sh
+++
@@ -1,2 +0,0 @@
-module load intelFPGA_pro/18.1.1_max nalla_pcie/18.1.1_max
-g++ -fPIC -g main.cpp utility.cpp `aocl compile-config` `aocl link-config` -o channel 
