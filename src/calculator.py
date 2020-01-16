7df259c05accba0cefc87968497f69d64c405516
Johannes de Fine Licht <definelicht@inf.ethz.ch>
2020-01-16 17:59:19 +0100

    Changed license to BSD

--- src/calculator.py
+++ src/calculator.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-
-"""
-    StencilFlow
-    Copyright (C) 2018-2020  Andreas Kuster
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-__author__ = "Andreas Kuster"
-__copyright__ = "Copyright 2018-2020, StencilFlow"
-__license__ = "GPL"
-
 import ast
 import math
 import operator
