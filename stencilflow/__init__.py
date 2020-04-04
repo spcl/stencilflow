76bd2c7fd33bccad49c4ed55bf704f45d781785c
andreaskuster <mail@andreaskuster.ch>
2020-04-04 18:23:12 +0200

    Only expose relevant classes globally.

--- stencilflow/__init__.py
+++ stencilflow/__init__.py
@@ -4,17 +4,10 @@ import sys
 # Use local DaCe submodule
 sys.path.append(os.path.join(os.path.dirname(__file__), "dace"))
 
-from .log_level import LogLevel
-from .kernel_chain_graph import KernelChainGraph
-from .compute_graph import ComputeGraph
-from .sdfg_generator import generate_sdfg
 from .sdfg_to_stencilflow import canonicalize_sdfg, sdfg_to_stencilflow
-from .optimizer import Optimizer
-from .simulator import Simulator
 from .kernel import Kernel
-from .bounded_queue import BoundedQueue
-from .helper import helper
-from .input import Input
-from .output import Output
-from .calculator import Calculator
+from .simulator import Simulator
+from .optimizer import Optimizer
+from .kernel_chain_graph import KernelChainGraph
 from .run_program import run_program
+from .sdfg_generator import generate_sdfg
