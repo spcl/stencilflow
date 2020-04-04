4c5395b8a195788ceea71d854b2a1b01c0969922
andreaskuster <mail@andreaskuster.ch>
2020-04-05 00:24:54 +0200

    Fix import. #2

--- stencilflow/kernel_chain_graph.py
+++ stencilflow/kernel_chain_graph.py
@@ -577,7 +577,7 @@ class KernelChainGraph:
                 )))
 
         print("instantiate optimizer...")
-        from .optimizer import Optimizer
+        from stencilflow import Optimizer
         opt = Optimizer(self.kernel_nodes, self.dimensions)
         bound = 12001
         opt.minimize_fast_mem(communication_volume_bound=bound)
