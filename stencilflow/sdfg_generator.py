c088925b011931d9a206fb7124e25a5aa50a7ad6
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-04-17 15:21:21 +0200

    Scalar containers for constants_prop

--- stencilflow/sdfg_generator.py
+++ stencilflow/sdfg_generator.py
@@ -213,8 +213,7 @@ def generate_sdfg(name, chain):
     sdfg = SDFG(name)
 
     for k, v in chain.constants.items():
-        sdfg.add_constant(k, v["value"],
-                          stencilflow.str_to_dtype(v["data_type"]))
+        sdfg.add_constant(k, v["value"], dace.data.Scalar(v["data_type"]))
 
     pre_state = sdfg.add_state("initialize")
     state = sdfg.add_state("compute")
