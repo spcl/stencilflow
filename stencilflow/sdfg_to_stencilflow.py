d143c080aa8e1f92d19652380b51185f164f8078
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-04-18 22:28:44 +0200

    Bug in one-dimensional array

--- stencilflow/sdfg_to_stencilflow.py
+++ stencilflow/sdfg_to_stencilflow.py
@@ -443,9 +443,7 @@ class _RenameTransformer(ast.NodeTransformer):
 
     @staticmethod
     def _offset_to_index(node, offset, iterator):
-        if isinstance(node, int):
-            return node
-        elif isinstance(node, ast.Num):
+        if isinstance(node, ast.Num):
             num = int(node.n)
             if num == 0:
                 return iterator
@@ -474,7 +472,7 @@ class _RenameTransformer(ast.NodeTransformer):
         else:
             # One dimensional access doesn't show up as a tuple
             indices = (_RenameTransformer._offset_to_index(
-                node.slice.value.n, offsets[0], iterators[0]), )
+                node.slice.value, offsets[0], iterators[0]), )
         indices = "({})".format(", ".join(map(str, indices)))
         node.slice.value = ast.parse(str(indices)).body[0].value
         self.generic_visit(node)
