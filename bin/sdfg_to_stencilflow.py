f859e1b477691b9b4deb29eddf0e270d592e1e4e
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-04-19 19:54:35 +0200

    Pass vector length

--- bin/sdfg_to_stencilflow.py
+++ bin/sdfg_to_stencilflow.py
@@ -12,9 +12,13 @@ if __name__ == "__main__":
     parser.add_argument("input_sdfg")
     parser.add_argument("output_json")
     parser.add_argument("-data-directory")
+    parser.add_argument("-vector-length", type=int, default=1)
 
     args = parser.parse_args()
 
-    sdfg_to_stencilflow(args.input_sdfg, args.output_json, args.data_directory)
+    sdfg_to_stencilflow(args.input_sdfg,
+                        args.output_json,
+                        vector_length=args.vector_length,
+                        data_directory=args.data_directory)
 
     print("Saved StencilFlow program to: {}".format(args.output_json))
