9fe0300421db8ff6210d9da1fd8a5fde8ef5a309
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-02 18:52:25 +0200

    Nest pip installs just in case

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -3,5 +3,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 (cd $DIR && git submodule update --init --recursive)
 (cd $DIR && virtualenv venv)
 source $DIR/venv/bin/activate
-$DIR/venv/bin/pip install -r requirements.txt
-$DIR/venv/bin/pip install --editable dace
+(cd $DIR && $DIR/venv/bin/pip install -r requirements.txt)
+(cd $DIR && $DIR/venv/bin/pip install --editable dace)
