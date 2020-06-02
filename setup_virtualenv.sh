fb89b2af5c46cd71fbc059c892699bc7258506cb
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-02 18:49:11 +0200

    Source outside nested command

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -2,6 +2,6 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 (cd $DIR && git submodule update --init --recursive)
 (cd $DIR && virtualenv venv)
-(cd $DIR && source venv/bin/activate)
-(cd $DIR && venv/bin/pip install -r requirements.txt)
-(cd $DIR && venv/bin/pip install --editable dace)
+source $DIR/venv/bin/activate
+$DIR/venv/bin/pip install -r requirements.txt
+$DIR/venv/bin/pip install --editable dace
