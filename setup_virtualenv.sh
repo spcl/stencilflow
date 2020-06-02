fb9584a4cf2379e5958d7aa2a26905efc65a588c
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-02 18:16:03 +0200

    Also install StencilFlow requirements

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -3,4 +3,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 (cd $DIR && git submodule update --init --recursive)
 (cd $DIR && virtualenv venv)
 (cd $DIR && source venv/bin/activate)
+(cd $DIR && pip install -r requirements.txt)
 (cd $DIR && pip install --editable dace)
