4ec68bc593e1c7bcf051d47c1f96024a1135ef03
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-02 18:42:32 +0200

    Try yet again

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -3,5 +3,5 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 (cd $DIR && git submodule update --init --recursive)
 (cd $DIR && virtualenv venv)
 (cd $DIR && source venv/bin/activate)
-(cd $DIR && pip install -r requirements.txt)
-(cd $DIR && pip install --editable dace)
+(cd $DIR && venv/bin/pip install -r requirements.txt)
+(cd $DIR && venv/bin/pip install --editable dace)
