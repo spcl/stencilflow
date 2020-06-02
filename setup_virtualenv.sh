c8123f6b719d4f7ab2e3c7e119fc18694832bce4
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-02 16:51:41 +0200

    Recurse submodule init

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
-(cd $DIR && git submodule update --init)
+(cd $DIR && git submodule update --init --recursive)
 (cd $DIR && virtualenv venv)
 (cd $DIR && source venv/bin/activate)
 (cd $DIR && pip install --editable dace)
