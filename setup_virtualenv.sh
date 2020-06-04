34180947092359464a5b796a5fd4ab3915f8dfe8
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-06-04 16:34:02 +0200

    Force python 3

--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
 (cd $DIR && git submodule update --init --recursive)
-(cd $DIR && virtualenv venv)
+(cd $DIR && virtualenv -p python3 venv)
 source $DIR/venv/bin/activate
 (cd $DIR && $DIR/venv/bin/pip install -r requirements.txt)
 (cd $DIR && $DIR/venv/bin/pip install --editable dace)
